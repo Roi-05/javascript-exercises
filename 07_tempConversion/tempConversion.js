295d71d1a168b490635920c9e0932b63044da912
John Roi <03jrd05@gmail.com>
2025-02-19 17:33:21 +0800

    Finish TOP assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celcius = (temperature - 32) * 5/9;
+  let roundedCelcius = Math.round(celcius * 10) / 10;
+  return roundedCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let farenheit = temperature * 9/5 + 32;
+  let roundedFarenheit = Math.round(farenheit * 10) / 10;
+  return roundedFarenheit;
 };
 
 // Do not edit below this line
