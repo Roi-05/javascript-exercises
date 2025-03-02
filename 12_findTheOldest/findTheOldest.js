698de918a8001630009ce4fd96ffc595d5c99f7f
John Roi <03jrd05@gmail.com>
2025-03-02 19:47:55 +0800

    Finish this

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function getAge (birth, death) {
+    if (death === undefined) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b)  => {
+        let currentGuy = getAge(a.yearOfBirth, a.yearOfDeath);
+        let nextGuy = getAge(b.yearOfBirth, b.yearOfDeath);
+
+        return (currentGuy > nextGuy) ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
