d3b3f53fd22dc0fb354e9568f89360ca74e3efc8
John Roi <03jrd05@gmail.com>
2025-03-02 16:09:09 +0800

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+const abcs = '1234567890qwertyuiopasdfghjklzxcvbnm';
 
+const filtered = string.toLowerCase().split('').filter((string) => abcs.includes(string)).join('');
+const reversed = filtered.split('').reverse().join('');
+
+return reversed === filtered;
 };
 
 // Do not edit below this line
